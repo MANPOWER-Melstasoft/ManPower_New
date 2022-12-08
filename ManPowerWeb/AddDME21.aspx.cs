9c99d211b08260762531d923b2bc24d84fcca3af
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2022-12-08 11:54:03 +0530

    update ( deleted test files )

--- ManPowerWeb/AddDME21.aspx.cs
+++ ManPowerWeb/AddDME21.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ManPowerCore.Common;
+using ManPowerCore.Controller;
+using ManPowerCore.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,13 @@ namespace ManPowerWeb
 {
     public partial class AddDME21 : System.Web.UI.Page
     {
+        List<TaskType> taskTypeList = new List<TaskType>();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            TaskTypeController taskTypeController = ControllerFactory.CreateTaskTypeController();
+            taskTypeList = taskTypeController.GetAllTaskType(false);
+            ddlWorkType.DataSource = taskTypeList;
+            ddlWorkType.DataBind();
         }
     }
 }
--- ManPowerWeb/AddDME21.aspx.designer.cs
+++ ManPowerWeb/AddDME21.aspx.designer.cs
@@ -33,6 +33,51 @@ namespace ManPowerWeb
         protected global::System.Web.UI.WebControls.DropDownList ddlWorkType;
 
         /// <summary>
+        /// ProgramDisplay control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl ProgramDisplay;
+
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// ddlProgram control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlProgram;
+
+        /// <summary>
+        /// Other control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Other;
+
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>

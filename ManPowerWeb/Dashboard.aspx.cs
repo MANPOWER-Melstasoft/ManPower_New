32caa409069a929426e1071d412e475d7cefb3b8
Charith Anjana <charithanjana01@gmail.com>
2022-12-15 19:05:17 +0530

    done home page

--- ManPowerWeb/Dashboard.aspx.cs
+++ ManPowerWeb/Dashboard.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ManPowerCore.Common;
+using ManPowerCore.Controller;
+using ManPowerCore.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,38 @@ namespace ManPowerWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["UserId"] != null)
+            {
+                if (!IsPostBack)
+                {
+                    BindCardData();
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
         }
+
+        protected void BindCardData()
+        {
+            SystemUserController systemUserController = ControllerFactory.CreateSystemUserController();
+            List<SystemUser> systemUserList = systemUserController.GetAllSystemUser(false, false, false);
+            lblNumberOfEmp.Text = systemUserList.Count.ToString();
+
+            ProgramTargetController programTargetController = ControllerFactory.CreateProgramTargetController();
+            List<ProgramTarget> programTargetsList = programTargetController.GetAllProgramTarget(false, false, false, false);
+            int mCount = 0;
+            foreach (var i in programTargetsList)
+            {
+                if (i.TargetMonth == DateTime.Today.Month)
+                {
+                    mCount++;
+                }
+            }
+            lblThisMonthTarget.Text = mCount.ToString();
+        }
+
+
     }
 }

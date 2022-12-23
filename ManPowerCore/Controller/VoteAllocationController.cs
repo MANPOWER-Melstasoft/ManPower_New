75206c600a6d2c0865258a36d5a060b1cf8e518e
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2022-12-23 18:00:42 +0530

    Vote Drop down Done

--- ManPowerCore/Controller/VoteAllocationController.cs
+++ ManPowerCore/Controller/VoteAllocationController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ManPowerCore.Common;
+using ManPowerCore.Domain;
+using ManPowerCore.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,37 @@ namespace ManPowerCore.Controller
 {
     public interface VoteAllocationController
     {
+        List<VoteAllocation> getallVoteAllocation();
     }
     public class VoteAllocationControllerImpl : VoteAllocationController
     {
+        DBConnection dBConnection = null;
+        public List<VoteAllocation> getallVoteAllocation()
+        {
 
+            try
+            {
+                dBConnection = new DBConnection();
+                List<VoteAllocation> voteList = new List<VoteAllocation>();
+                VoteAllocationDAO dao = DAOFactory.CreateVoteAllocationDAO();
+
+                voteList = dao.getallVoteAllocation(dBConnection);
+                return voteList;
+
+            }
+            catch (Exception)
+            {
+                dBConnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dBConnection.con.State == System.Data.ConnectionState.Open)
+                    dBConnection.Commit();
+            }
+
+
+        }
     }
+
 }

a405c9eeded7c397e381df0d7e74744d2ce85c27
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-01-04 00:23:10 +0530

    approve leave search done

--- ManPowerWeb/ApproveLeave.aspx.cs
+++ ManPowerWeb/ApproveLeave.aspx.cs
@@ -16,6 +16,9 @@ namespace ManPowerWeb
         List<DepartmentUnit> departmentUnitsList = new List<DepartmentUnit>();
         List<Employee> employeesList = new List<Employee>();
         List<StaffLeave> staffLeaveList = new List<StaffLeave>();
+        List<StaffLeave> staffLeaveSearchList = new List<StaffLeave>();
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -37,6 +40,7 @@ namespace ManPowerWeb
             ddlDistrict.DataSource = departmentUnitsList.Where(x => x.DepartmentUnitTypeId == 2);
             ddlDistrict.DataValueField = "DepartmentUnitId";
             ddlDistrict.DataTextField = "Name";
+            ddlDistrict.Items.Insert(0, new ListItem("Select District", ""));
 
 
             ddlHo.DataBind();
@@ -48,6 +52,8 @@ namespace ManPowerWeb
 
 
             staffLeaveList = staffLeaveController.getStaffLeaves(true);
+
+            ViewState["staffLeaveList"] = staffLeaveList.ToList();
 
 
 
@@ -71,6 +77,7 @@ namespace ManPowerWeb
             ddlDS.DataValueField = "DepartmentUnitId";
             ddlDS.DataTextField = "Name";
             ddlDS.DataBind();
+            ddlDistrict.Items.Insert(0, new ListItem("Select DS Division", ""));
 
 
 
@@ -91,6 +98,32 @@ namespace ManPowerWeb
 
             Response.Redirect("ApproveLeaveView.aspx?EmpId=" + staffLeaveList[rowIndex].EmployeeId.ToString() + "&Id=" + staffLeaveList[rowIndex].StaffLeaveId);
 
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+
+
+            staffLeaveSearchList = (List<StaffLeave>)ViewState["staffLeaveList"];
+
+            if (ddlDistrict.SelectedValue != "0")
+            {
+                if (ddlDS.SelectedValue != "0")
+                {
+                    staffLeaveSearchList = staffLeaveSearchList.Where(x => x._EMployeeDetails.DistrictId == Convert.ToInt32(ddlDistrict.SelectedValue) && x._EMployeeDetails.DSDivisionId == Convert.ToInt32(ddlDS.SelectedValue)).ToList();
+
+                }
+                else
+                {
+                    staffLeaveSearchList = staffLeaveSearchList.Where(x => x._EMployeeDetails.DistrictId == Convert.ToInt32(ddlDistrict.SelectedValue)).ToList();
+
+                }
+            }
+
+            gvApproveLeave.DataSource = staffLeaveSearchList;
+            gvApproveLeave.DataBind();
 
         }
     }

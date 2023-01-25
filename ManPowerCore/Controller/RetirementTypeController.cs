6ca97609764c30a40e018204b5bf59414a7d3afd
Charith Anjana <charithanjana01@gmail.com>
2023-01-26 00:32:01 +0530

    add RetirementType class

---
+++ ManPowerCore/Controller/RetirementTypeController.cs
@@ -0,0 +1,39 @@
+﻿using ManPowerCore.Common;
+using ManPowerCore.Domain;
+using ManPowerCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManPowerCore.Controller
+{
+    public interface RetirementTypeController
+    {
+        List<RetirementType> GetAllRetirementType(bool with0);
+    }
+
+    public class RetirementTypeControllerSqlImpl : RetirementTypeController
+    {
+        public List<RetirementType> GetAllRetirementType(bool with0)
+        {
+            DBConnection dbConnection = new DBConnection();
+            try
+            {
+                RetirementTypeDAO DAO = DAOFactory.CreateRetirementTypeDAO();
+                return DAO.GetAllRetirementType(with0, dbConnection);
+            }
+            catch (Exception ex)
+            {
+                dbConnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dbConnection.con.State == System.Data.ConnectionState.Open)
+                    dbConnection.Commit();
+            }
+        }
+    }
+}
---
+++ ManPowerCore/Domain/RetirementType.cs
@@ -0,0 +1,22 @@
+﻿using ManPowerCore.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManPowerCore.Domain
+{
+    [Serializable]
+    public class RetirementType
+    {
+        [DBField("Id")]
+        public int Id { get; set; }
+
+        [DBField("Name")]
+        public string RetirementTypeName { get; set; }
+
+        [DBField("Is_Active")]
+        public string Is_Active { get; set; }
+    }
+}

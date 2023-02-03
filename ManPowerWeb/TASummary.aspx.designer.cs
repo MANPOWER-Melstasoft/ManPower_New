4a6e13ae1c091ad261a31428ad7c3d9711f11ba2
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2023-02-03 23:18:23 +0530

    Individual TA Summary Report

--- ManPowerWeb/TASummary.aspx.designer.cs
+++ ManPowerWeb/TASummary.aspx.designer.cs
@@ -15,12 +15,12 @@ namespace ManPowerWeb
     {
 
         /// <summary>
-        /// gvTAIndividualSummary control.
+        /// gvIndividualTASummary control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvTAIndividualSummary;
+        protected global::System.Web.UI.WebControls.GridView gvIndividualTASummary;
     }
 }

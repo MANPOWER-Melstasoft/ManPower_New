4acb8da191255bc879a93bcabae816280900c1bb
Charith Anjana <charithanjana01@gmail.com>
2023-02-05 22:21:58 +0530

    update

--- ManPowerWeb/ApproveTransfersRetirementResignationView.aspx.cs
+++ ManPowerWeb/ApproveTransfersRetirementResignationView.aspx.cs
@@ -19,16 +19,18 @@ namespace ManPowerWeb
         static List<SystemUser> AssignUserList = new List<SystemUser>();
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+
             if (!IsPostBack)
             {
                 if (Request.QueryString["Id"] != null)
                 {
                     Id = Convert.ToInt32(Request.QueryString["Id"]);
-                    BindData();
                     BindStatus();
                     BindAction();
                     BindAssignUser();
                     BindReverseReason();
+                    BindData();
                 }
                 else
                 {
@@ -55,6 +57,59 @@ namespace ManPowerWeb
             lblDesignation.Text = systemUser._Designation.DesigntionName;
             lblDocument.Text = trrmainObj.Documents;
             document = trrmainObj.Documents;
+
+            if (trrmainObj.StatusId == 2)
+            {
+                ddlUpdateStatus.SelectedValue = "1";
+            }
+            else if (trrmainObj.StatusId == 3)
+            {
+                ddlUpdateStatus.SelectedValue = "3";
+                sendtoapp.Visible = false;
+                reverse.Visible = false;
+                reject.Visible = true;
+
+                txtRejectRemark.Text = trrmainObj.Remarks;
+
+            }
+            else if (trrmainObj.StatusId == 4)
+            {
+                ddlUpdateStatus.SelectedValue = "4";
+                sendtoapp.Visible = false;
+                reverse.Visible = true;
+                reject.Visible = false;
+
+                ReverseReasonController reverseReasonController = ControllerFactory.CreateReverseReasonController();
+                List<ReverseReason> reverseReasonsList = reverseReasonController.GetAllReverseReason(false);
+
+                ReverseReason reverseReasonsObj = new ReverseReason();
+
+                foreach (var item in reverseReasonsList)
+                {
+                    if (item.ReverseReasonName.ToLower() == trrmainObj.ParentAction.ToLower())
+                    {
+                        reverseReasonsObj = item;
+                    }
+                }
+
+                if (reverseReasonsObj != null)
+                {
+                    ddlReverseReason.SelectedValue = Convert.ToString(reverseReasonsObj.Id);
+                }
+
+            }
+            else if (trrmainObj.StatusId == 5)
+            {
+                if (Convert.ToInt32(Session["UserTypeId"]) == 1)
+                {
+                    ddlUpdateStatus.SelectedValue = "0";
+                }
+                else
+                {
+                    ddlUpdateStatus.SelectedValue = "1";
+                }
+
+            }
 
             if (trrmainObj.RequestTypeId == 1)
             {
@@ -122,24 +177,24 @@ namespace ManPowerWeb
             trrmainObj.Remarks = "";
             trrmainObj.Reason = "";
 
-            if (ddlUpdateStatus.SelectedValue == "Approve")
+            if (ddlUpdateStatus.SelectedItem.Text == "Approve")
             {
                 trrmainObj.StatusId = 2;
                 trrmainObj.ParentAction = "Approve";
             }
-            if (ddlUpdateStatus.SelectedValue == "Send to Approval")
+            if (ddlUpdateStatus.SelectedItem.Text == "Send to Approval")
             {
                 trrmainObj.StatusId = 5;
                 SystemUser systemUser = AssignUserList.Where(x => x.SystemUserId == Convert.ToInt32(ddlAssignUser.SelectedValue)).Single();
                 trrmainObj.RecomendParentId = systemUser._DepartmentUnitPositions.DepartmetUnitPossitionsId;
                 trrmainObj.ParentAction = ddlAction.SelectedItem.Text;
             }
-            if (ddlUpdateStatus.SelectedValue == "Reverse")
+            if (ddlUpdateStatus.SelectedItem.Text == "Reverse")
             {
                 trrmainObj.StatusId = 4;
                 trrmainObj.ParentAction = ddlReverseReason.SelectedItem.Text;
             }
-            if (ddlUpdateStatus.SelectedValue == "Reject")
+            if (ddlUpdateStatus.SelectedItem.Text == "Reject")
             {
                 trrmainObj.StatusId = 3;
                 trrmainObj.ParentAction = "Reject";
@@ -203,22 +258,18 @@ namespace ManPowerWeb
 
         private void BindStatus()
         {
-            List<string> list = new List<string>();
+            ddlUpdateStatus.Items.Insert(0, new ListItem("-- select status --", ""));
 
             if (Convert.ToInt32(Session["UserTypeId"]) == 1)
             {
-                list.Add("Approve");
+                ddlUpdateStatus.Items.Insert(1, new ListItem("Approve", "1"));
             }
             else
             {
-                list.Add("Send to Approval");
+                ddlUpdateStatus.Items.Insert(1, new ListItem("Send to Approval", "1"));
             }
-            list.Add("Reverse");
-            list.Add("Reject");
-
-            ddlUpdateStatus.DataSource = list;
-            ddlUpdateStatus.DataBind();
-            ddlUpdateStatus.Items.Insert(0, new ListItem("-- select status --", ""));
+            ddlUpdateStatus.Items.Insert(2, new ListItem("Reverse", "4"));
+            ddlUpdateStatus.Items.Insert(3, new ListItem("Reject", "3"));
         }
 
         private void BindAction()
@@ -260,19 +311,25 @@ namespace ManPowerWeb
 
         protected void ddlUpdateStatus_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (ddlUpdateStatus.SelectedValue == "Send to Approval")
+            if (ddlUpdateStatus.SelectedItem.Text == "Approve")
+            {
+                sendtoapp.Visible = false;
+                reverse.Visible = false;
+                reject.Visible = false;
+            }
+            if (ddlUpdateStatus.SelectedItem.Text == "Send to Approval")
             {
                 sendtoapp.Visible = true;
                 reverse.Visible = false;
                 reject.Visible = false;
             }
-            if (ddlUpdateStatus.SelectedValue == "Reverse")
+            if (ddlUpdateStatus.SelectedItem.Text == "Reverse")
             {
                 sendtoapp.Visible = false;
                 reverse.Visible = true;
                 reject.Visible = false;
             }
-            if (ddlUpdateStatus.SelectedValue == "Reject")
+            if (ddlUpdateStatus.SelectedItem.Text == "Reject")
             {
                 sendtoapp.Visible = false;
                 reverse.Visible = false;

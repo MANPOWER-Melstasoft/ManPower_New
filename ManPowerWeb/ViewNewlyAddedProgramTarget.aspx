98a43ba4889fab707b8359ebd50391ecf8ee60d0
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-02-07 18:04:51 +0530

    ViewNewlyAddedProgramTarget created

---
+++ ManPowerWeb/ViewNewlyAddedProgramTarget.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ViewNewlyAddedProgramTarget.aspx.cs" Inherits="ManPowerWeb.ViewNewlyAddedProgramTarget" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="card p-4 mb-4 ">
+            <div class="table-responsive" style="width: 100%; padding-left: 40px; padding-right: 40px;">
+                <asp:GridView runat="server" ID="gvProgramTargetNotification"
+                    Style="margin-top: 30px;" AutoGenerateColumns="False" CssClass="table table-bordered"
+                    CellPadding="4" GridLines="None" HeaderStyle-HorizontalAlign="Center">
+
+                    <Columns>
+                        <asp:BoundField DataField="Title" HeaderText="Program Target Name" ItemStyle-HorizontalAlign="Center" HeaderStyle-CssClass="table-dark" />
+                        <asp:BoundField DataField="RecommendedDate" HeaderText="Date Recommended" ItemStyle-HorizontalAlign="Center" HeaderStyle-CssClass="table-dark" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ ManPowerWeb/ViewNewlyAddedProgramTarget.aspx.cs
@@ -0,0 +1,45 @@
+﻿using ManPowerCore.Common;
+using ManPowerCore.Controller;
+using ManPowerCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManPowerWeb
+{
+    public partial class ViewNewlyAddedProgramTarget : System.Web.UI.Page
+    {
+
+
+        List<ProgramTarget> programTargetsList = new List<ProgramTarget>();
+        List<DepartmentUnitPositions> DepartmentUnitPositionsList = new List<DepartmentUnitPositions>();
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                bindDataSource();
+            }
+        }
+
+        private void bindDataSource()
+        {
+            int systemUserId = Convert.ToInt32(Session["UserId"]);
+
+            DepartmentUnitPositionsList = ControllerFactory.CreateDepartmentUnitPositionsController().GetAllUsersBySystemUserId(systemUserId);
+
+            int departmentUnitPositionId = DepartmentUnitPositionsList[0].DepartmetUnitPossitionsId;
+
+            programTargetsList = ControllerFactory.CreateProgramTargetController().GetAllProgramTarget(false, false, true, false);
+
+            gvProgramTargetNotification.DataSource = programTargetsList.Where(x => x.IsRecommended == 2 && x._ProgramAssignee[0].DepartmentUnitPossitionsId == departmentUnitPositionId && x._ProgramAssignee[0].Is_View == 0);
+            gvProgramTargetNotification.DataBind();
+
+        }
+    }
+}

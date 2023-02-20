7970483fff24509597ce007c5c73210a54b8f0ce
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2023-02-20 23:18:56 +0530

    Request Loan Front added

---
+++ ManPowerWeb/RequestLoanFront.aspx.cs
@@ -0,0 +1,39 @@
+﻿using ManPowerCore.Common;
+using ManPowerCore.Controller;
+using ManPowerCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManPowerWeb
+{
+    public partial class RequestLoanFront : System.Web.UI.Page
+    {
+        List<LoanDetail> loanDetailList = new List<LoanDetail>();
+        LoanDetailsController loanDetailsController = ControllerFactory.CreateLoanDetailsController();
+        public int EmpId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmpId = Convert.ToInt32(Session["EmpNumber"]);
+            BindDataSource();
+        }
+
+        public void BindDataSource()
+        {
+            loanDetailList = loanDetailsController.GetAllLoanDetailWithStatus(true, true);
+            loanDetailList = loanDetailList.Where(x => x.EmployeeId == EmpId).ToList();
+
+            gvAppliedLoan.DataSource = loanDetailList;
+            gvAppliedLoan.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string url = "RequestLoan.aspx";
+            Response.Redirect(url);
+        }
+    }
+}

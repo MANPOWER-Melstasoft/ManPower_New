1e3bc75ed604ca518125aef2ac56de0ed29eb00b
Charith Anjana <charithanjana01@gmail.com>
2023-03-03 12:05:07 +0530

    bug fix in request maintain and approve maintain

--- ManPowerWeb/MaintenanceApproval.aspx.designer.cs
+++ ManPowerWeb/MaintenanceApproval.aspx.designer.cs
@@ -42,13 +42,13 @@ namespace ManPowerWeb
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
-        /// Button2 control.
+        /// btnReset control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button btnReset;
 
         /// <summary>
         /// GridView1 control.

fea9a8baec1e411a24e53b8e6b844a07c67171b6
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-03-21 11:54:31 +0530

    EmployeeDetailsFromProgramPlanController Created

--- ManPowerCore/Controller/EmployeeDetailsFromProgramPlanController.cs
+++ ManPowerCore/Controller/EmployeeDetailsFromProgramPlanController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ManPowerCore.Common;
+using ManPowerCore.Domain;
+using ManPowerCore.Infrastructure;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +12,52 @@ namespace ManPowerCore.Controller
 {
     public interface EmployeeDetailsFromProgramPlanController
     {
+        List<EmployeeDetailsFromProgramPlan> GetAllEmployeeDetailsFromProgramPlans();
 
+        EmployeeDetailsFromProgramPlan GetAllEmployeeDetailsFromProgramPlansByProgramPlanId(int ProgramPlanId);
     }
     public class EmployeeDetailsFromProgramPlanImpl : EmployeeDetailsFromProgramPlanController
     {
+        DBConnection dbConnection;
+        EmployeeDetailsFromProgramPlanDAO employeeDetailsFromProgramPlanDAO = DAOFactory.CreateemployeeDetailsFromProgramPlanDAO();
+        public List<EmployeeDetailsFromProgramPlan> GetAllEmployeeDetailsFromProgramPlans()
+        {
+            try
+            {
+                dbConnection = new DBConnection();
+                return employeeDetailsFromProgramPlanDAO.GetAllEmployeeDetailsFromProgramPlans(dbConnection);
+            }
 
+            catch (Exception)
+            {
+                dbConnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dbConnection.con.State == System.Data.ConnectionState.Open)
+                    dbConnection.Commit();
+            }
+        }
+
+        public EmployeeDetailsFromProgramPlan GetAllEmployeeDetailsFromProgramPlansByProgramPlanId(int ProgramPlanId)
+        {
+            try
+            {
+                dbConnection = new DBConnection();
+                return employeeDetailsFromProgramPlanDAO.GetAllEmployeeDetailsFromProgramPlansByProgramPlanId(dbConnection, ProgramPlanId);
+            }
+
+            catch (Exception)
+            {
+                dbConnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dbConnection.con.State == System.Data.ConnectionState.Open)
+                    dbConnection.Commit();
+            }
+        }
     }
 }

0752967198f2c5ddf0731a4ffcc010a2ed9d194f
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-04-26 17:46:48 +0530

    Maintenance Request Recommendation Data Binding

--- ManPowerWeb/MaintenanceRecomandView.aspx.cs
+++ ManPowerWeb/MaintenanceRecomandView.aspx.cs
@@ -3,6 +3,7 @@ using ManPowerCore.Controller;
 using ManPowerCore.Domain;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,69 +16,113 @@ namespace ManPowerWeb
         List<VehicleMeintenance> vehicleMeintenances = new List<VehicleMeintenance>();
         List<SystemUser> systemUsers = new List<SystemUser>();
         List<SystemUser> fiterList = new List<SystemUser>();
+        List<MaintenanceCategory> maintenanceCategories = new List<MaintenanceCategory>();
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
             this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
 
-
-            MaintenanceCategoryController maintenanceCategoryController = ControllerFactory.CreateMaintenanceCategoryController();
-
-            VehicleMaintenanceController vehicleMaintenanceController = ControllerFactory.CreateVehicleMaintenanceController();
-            vehicleMeintenances = vehicleMaintenanceController.GetAllVehicleMeintenance();
-
-            SystemUserController systemUserController = ControllerFactory.CreateSystemUserController();
-            systemUsers = systemUserController.GetAllSystemUser(false, false, false);
-
-            string id = Request.QueryString["id"];
-
-            butonA.Visible = false;
-            butonR.Visible = false;
-
-            foreach (var j in systemUsers.Where(u => u.SystemUserId == Convert.ToInt32(Session["UserId"])))
+            if (!IsPostBack)
             {
-                foreach (var i in vehicleMeintenances.Where(u => u.VehicleMeintenanceId == int.Parse(id)))
+
+                dropDownBind();
+
+
+
+                VehicleMaintenanceController vehicleMaintenanceController = ControllerFactory.CreateVehicleMaintenanceController();
+                vehicleMeintenances = vehicleMaintenanceController.GetAllVehicleMeintenance();
+
+                SystemUserController systemUserController = ControllerFactory.CreateSystemUserController();
+                systemUsers = systemUserController.GetAllSystemUser(false, false, false);
+
+                string id = Request.QueryString["id"];
+
+                butonA.Visible = false;
+                butonR.Visible = false;
+
+
+                VehicleMeintenance i = vehicleMeintenances.Where(u => u.VehicleMeintenanceId == int.Parse(id)).Single();
+
+                fielNo.Text = i.FileNo;
+                date.Text = i.RequestDate.ToString();
+                requestedBy.Text = i.Employee.NameWithInitials.ToString();
+                vNo.Text = i.VehicleNumber;
+                description.Text = i.RequestDescription.ToString();
+                txtMeter.Text = i.VehicleMeter;
+                txtMiladge.Text = i.Mileage;
+                ddlCategory.SelectedValue = i.CategoryId.ToString();
+                txtMeter.Text = i.VehicleMeter;
+                txtPrevMeter.Text = i.VehiclePrevMeter;
+
+                if (ddlCategory.SelectedValue == "4" && i.InsuranceStartDate.Year != 1 && i.InsuranceEndDate.Year != 1)
                 {
-                    fielNo.Text = i.FileNo;
-                    date.Text = i.RequestDate.ToString();
-                    requestedBy.Text = i.Employee.NameWithInitials.ToString();
-                    vNo.Text = i.VehicleNumber;
-                    description.Text = i.RequestDescription.ToString();
-                    txtMeter.Text = i.VehicleMeter;
-                    txtMiladge.Text = i.Mileage;
+                    txtStartDate.Text = i.InsuranceStartDate.ToShortDateString();
+                    txtEndDate.Text = i.InsuranceEndDate.ToShortDateString();
+                }
 
-                    MaintenanceCategory maintenanceCategory = maintenanceCategoryController.GetMaintenanceCategory(i.CategoryId);
-                    category.Text = maintenanceCategory.MaintenanceCategoryName;
+                if (i.IsEngineerRecommendation == "1")
+                {
+                    chkEnginerrReommendation.Checked = true;
+                    rowEngFileUploader.Visible = true;
 
-                    if (i.IsApproved == 0)
+                    if (i.Attachment != "")
                     {
-                        butonA.Visible = true;
-                        butonR.Visible = true;
-
-                        approval.Text = "Not Recommended";
-                    }
-                    else if (i.IsApproved == 1)
-                    {
-                        approval.Text = "Pending Approval";
-                    }
-
-                    else if (i.IsApproved == 2)
-                    {
-                        approval.Text = "Request Approved";
-                    }
-
-                    else if (i.IsApproved == 3)
-                    {
-                        approval.Text = "Request Rejected";
+                        Label1.Text = i.Attachment;
+                        Doclink.HRef = "/SystemDocuments/Quatations/" + i.Attachment;
                     }
                 }
+                else
+                {
+                    chkEnginerrReommendation.Checked = false;
+                }
+
+                if (i.IsApproved == 0)
+                {
+                    butonA.Visible = true;
+                    butonR.Visible = true;
+
+                    approval.Text = "Not Recommended";
+                }
+                else if (i.IsApproved == 1)
+                {
+                    approval.Text = "Pending Approval";
+                }
+
+                else if (i.IsApproved == 2)
+                {
+                    approval.Text = "Request Approved";
+                }
+
+                else if (i.IsApproved == 3)
+                {
+                    approval.Text = "Request Rejected";
+                }
+
+
             }
+
+
+
 
         }
 
         protected void isClicked(object sender, EventArgs e)
         {
             Response.Redirect("MaintenanceRecomand.aspx");
+        }
+
+        private void dropDownBind()
+        {
+            MaintenanceCategoryController maintenanceCategoryController = ControllerFactory.CreateMaintenanceCategoryController();
+            MaintenanceCategoryController maintenanceCategory = ControllerFactory.CreateMaintenanceCategoryController();
+            maintenanceCategories = maintenanceCategory.GetAllMaintenanceCategory();
+
+            ddlCategory.DataSource = maintenanceCategories;
+            ddlCategory.DataTextField = "MaintenanceCategoryName";
+            ddlCategory.DataValueField = "MaintenanceCategoryId";
+            ddlCategory.DataBind();
+            ddlCategory.Items.Insert(0, new ListItem("-- Select --", ""));
         }
 
         protected void Accept(object sender, EventArgs e)

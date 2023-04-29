7a118b619026b75210a1fa661a24e5e129388926
Parami98 <paraminawodya500@gmail.com>
2023-04-29 11:01:15 +0530

    Prefered workplace dropdown change in AddTransfersRetirementResignation

--- ManPowerWeb/AddTransfersRetirementResignation.aspx.cs
+++ ManPowerWeb/AddTransfersRetirementResignation.aspx.cs
@@ -375,7 +375,7 @@ namespace ManPowerWeb
             ddlDepartment.DataValueField = "DepartmentUnitId";
             ddlDepartment.DataTextField = "Name";
             ddlDepartment.DataBind();
-            ddlDepartment.Items.Insert(0, new ListItem("-- department --", ""));
+            ddlDepartment.Items.Insert(0, new ListItem("-- Select prefered work place --", ""));
         }
 
         private void BindTransferType()

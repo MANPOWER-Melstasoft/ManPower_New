4832e1e9d98fc39097af111c83baa5e93354d521
vPankaja <vpankaja42@gmail.com>
2023-05-15 13:20:29 +0530

    User Training Report Commit

--- ManPowerWeb/MaintenanceRecomand.aspx.cs
+++ ManPowerWeb/MaintenanceRecomand.aspx.cs
@@ -10,69 +10,69 @@ using System.Web.UI.WebControls;
 
 namespace ManPowerWeb
 {
-    public partial class MaintenanceRecomand : System.Web.UI.Page
-    {
-        List<VehicleMeintenance> vehicleMeintenances = new List<VehicleMeintenance>();
-        List<VehicleMeintenance> searchList = new List<VehicleMeintenance>();
-        List<VehicleMeintenance> UserSearchList = new List<VehicleMeintenance>();
-        List<MaintenanceCategory> categries = new List<MaintenanceCategory>();
+	public partial class MaintenanceRecomand : System.Web.UI.Page
+	{
+		List<VehicleMeintenance> vehicleMeintenances = new List<VehicleMeintenance>();
+		List<VehicleMeintenance> searchList = new List<VehicleMeintenance>();
+		List<VehicleMeintenance> UserSearchList = new List<VehicleMeintenance>();
+		List<MaintenanceCategory> categries = new List<MaintenanceCategory>();
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
 
-            if (!IsPostBack)
-            {
-                BindDataSource();
-            }
-        }
+			if (!IsPostBack)
+			{
+				BindDataSource();
+			}
+		}
 
-        protected void BindDataSource()
-        {
-            VehicleMaintenanceController vehicleMaintenanceController = ControllerFactory.CreateVehicleMaintenanceController();
-            vehicleMeintenances = vehicleMaintenanceController.GetAllVehicleMeintenance();
+		protected void BindDataSource()
+		{
+			VehicleMaintenanceController vehicleMaintenanceController = ControllerFactory.CreateVehicleMaintenanceController();
+			vehicleMeintenances = vehicleMaintenanceController.GetAllVehicleMeintenance();
 
-            MaintenanceCategoryController maintenanceCategoryController = ControllerFactory.CreateMaintenanceCategoryController();
-            categries = maintenanceCategoryController.GetAllMaintenanceCategory();
+			MaintenanceCategoryController maintenanceCategoryController = ControllerFactory.CreateMaintenanceCategoryController();
+			categries = maintenanceCategoryController.GetAllMaintenanceCategory();
 
-            ddlCategory.DataSource = categries;
-            ddlCategory.DataTextField = "MaintenanceCategoryName";
-            ddlCategory.DataValueField = "MaintenanceCategoryId";
-            ddlCategory.DataBind();
-            ddlCategory.Items.Insert(0, new ListItem("-- Select --", ""));
+			ddlCategory.DataSource = categries;
+			ddlCategory.DataTextField = "MaintenanceCategoryName";
+			ddlCategory.DataValueField = "MaintenanceCategoryId";
+			ddlCategory.DataBind();
+			ddlCategory.Items.Insert(0, new ListItem("-- Select --", ""));
 
-            searchList = vehicleMeintenances.Where(x => x.RecomandBy == Convert.ToInt32(Session["EmpNumber"]) && x.IsApproved > 0).ToList();
-
-
-            ViewState["searchList"] = searchList;
-            GridView1.DataSource = searchList;
-            GridView1.DataBind();
-
-        }
-
-        protected void btnSearch_Click(object sender, EventArgs e)
-        {
-            UserSearchList = (List<VehicleMeintenance>)ViewState["searchList"];
+			searchList = vehicleMeintenances.Where(x => /*x.RecomandBy == Convert.ToInt32(Session["EmpNumber"]) &&*/ x.IsApproved == 1).ToList();
 
 
-            if (ddlCategory.SelectedValue != "")
-            {
-                UserSearchList = UserSearchList.Where(x => x.CategoryId == Convert.ToInt32(ddlCategory.SelectedValue)).ToList();
-            }
+			ViewState["searchList"] = searchList;
+			GridView1.DataSource = searchList;
+			GridView1.DataBind();
 
-            if (date.Text != "")
-            {
-                UserSearchList = UserSearchList.Where(u => u.RequestDate.Date == DateTime.Parse(date.Text)).ToList();
-            }
-            GridView1.DataSource = UserSearchList;
-            GridView1.DataBind();
-        }
+		}
 
-        protected void btnReset_Click(object sender, EventArgs e)
-        {
-            ddlCategory.ClearSelection();
-            date.Text = null;
-        }
+		protected void btnSearch_Click(object sender, EventArgs e)
+		{
+			UserSearchList = (List<VehicleMeintenance>)ViewState["searchList"];
 
-    }
+
+			if (ddlCategory.SelectedValue != "")
+			{
+				UserSearchList = UserSearchList.Where(x => x.CategoryId == Convert.ToInt32(ddlCategory.SelectedValue)).ToList();
+			}
+
+			if (date.Text != "")
+			{
+				UserSearchList = UserSearchList.Where(u => u.RequestDate.Date == DateTime.Parse(date.Text)).ToList();
+			}
+			GridView1.DataSource = UserSearchList;
+			GridView1.DataBind();
+		}
+
+		protected void btnReset_Click(object sender, EventArgs e)
+		{
+			ddlCategory.ClearSelection();
+			date.Text = null;
+		}
+
+	}
 }
